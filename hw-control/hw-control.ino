4d22a45d6b7bc6534a1cf682992b1df2f95e9748
Caleb Hoststler <18chostetler@gmail.com>
2022-10-11 15:50:32 -0400

    Add serial interface, change to AccelStepper library

--- hw-control/hw-control.ino
+++ hw-control/hw-control.ino
@@ -1,13 +1,21 @@
+#include <AccelStepper.h>
+const int x_step_pin = 2;
+const int y_step_pin = 3;
+const int z_step_pin = 4;
 
-const int x_step = 2;
-const int y_step = 3;
-const int z_step = 4;
-
-const int x_dir = 5;
-const int y_dir = 6;
-const int z_dir = 7;
+const int x_dir_pin = 5;
+const int y_dir_pin = 6;
+const int z_dir_pin = 7;
 
 const int enable = 8;
+
+int x_step = LOW;
+int y_step = LOW;
+int z_step = LOW;
+
+int x_dir = 0;
+int y_dir = LOW;
+int z_dir = LOW;
 
 // Experimentally, this has to be greater than 500 when on full step
 const int step_microseconds = 500;
@@ -15,33 +23,46 @@ const int full_steps_per_revolution = floor(28.65 * 200);
 // 1=full step, 2=half step, 4=quarter, etc.
 const int pulses_per_step = 1;
 
+int x_pos = 0;
+int x_sp = 0;
+int x_t = micros();
+
+AccelStepper x_stepper = AccelStepper(AccelStepper::DRIVER, x_step_pin, x_dir_pin);
+
 void setup() {
-  pinMode(x_step, OUTPUT);
-  pinMode(y_step, OUTPUT);
-  pinMode(z_step, OUTPUT);
-  pinMode(x_dir, OUTPUT);
-  pinMode(y_dir, OUTPUT);
-  pinMode(z_dir, OUTPUT);
+  pinMode(x_step_pin, OUTPUT);
+  pinMode(y_step_pin, OUTPUT);
+  pinMode(z_step_pin, OUTPUT);
+  pinMode(x_dir_pin, OUTPUT);
+  pinMode(y_dir_pin, OUTPUT);
+  pinMode(z_dir_pin, OUTPUT);
+
+  Serial.begin(9600);
+  x_stepper.setMaxSpeed(1000);
+  x_stepper.setSpeed(800);
 }
 
 void loop() {
-  digitalWrite(x_dir, HIGH);
-  for(int x=0; x<full_steps_per_revolution*pulses_per_step; x++){
-    digitalWrite(x_step, HIGH);
-    delayMicroseconds(step_microseconds);
-    digitalWrite(x_step, LOW);
-    delayMicroseconds(step_microseconds);
+  // Check to see if we have received any commands
+  
+  if (Serial.available() > 0){
+    String command = Serial.readString();
+    command.trim();
+    Serial.print("Echo: '");
+    Serial.print(command);
+    Serial.println("'");
+    
+    if (command.indexOf("pos")>=0){
+      Serial.println("pos OK (00.00, 000.00)");
+    } else if (command.indexOf("trace")>=0) {
+      Serial.println("trace OK");
+    } else if (command.indexOf("set")>=0) {
+      Serial.println("set OK");
+    } else {
+      Serial.println("invalid");
+    }
+    Serial.println();
   }
-  delay(1000);
-
-  digitalWrite(x_dir, LOW);
-  for(int x=0; x<full_steps_per_revolution*pulses_per_step; x++){
-    digitalWrite(x_step, HIGH);
-    delayMicroseconds(step_microseconds);
-    digitalWrite(x_step, LOW);
-    delayMicroseconds(step_microseconds);
-  }
-
-  delay(1000);
+  x_stepper.runSpeed();
   
 }
