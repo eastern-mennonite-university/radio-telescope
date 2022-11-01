e9dd2b8dc6dc80634176a5ff49bb52cb3ffbaaa4
Caleb Hoststler <18chostetler@gmail.com>
2022-11-01 15:52:55 -0400

    Change serial input to parse space-separated arguments

--- hw-control/hw-control.ino
+++ hw-control/hw-control.ino
@@ -1,11 +1,11 @@
 #include <AccelStepper.h>
+#include <string.h>
+
 const int x_step_pin = 2;
 const int y_step_pin = 3;
-const int z_step_pin = 4;
 
 const int x_dir_pin = 5;
 const int y_dir_pin = 6;
-const int z_dir_pin = 7;
 
 const int enable = 8;
 
@@ -17,10 +17,16 @@ const int tenor[] = {740, 659, 587, 554, 494, 440, 494, 554};
 
 AccelStepper x_stepper = AccelStepper(AccelStepper::DRIVER, x_step_pin, x_dir_pin);
 AccelStepper y_stepper = AccelStepper(AccelStepper::DRIVER, y_step_pin, y_dir_pin);
-AccelStepper z_stepper = AccelStepper(AccelStepper::DRIVER, z_step_pin, z_dir_pin);
 
 int music = 0;
 
+// Azimuth: 0deg=North, 90deg=East, etc.
+float azimuth = 0;
+// Altitude: 0deg=Horizon, 90deg=Vertical
+float altitude = 0;
+
+char* command_args[10];
+char* command_arg;
 void setup() {
   Serial.begin(9600);
   x_stepper.setMaxSpeed(1000);
@@ -31,23 +37,45 @@ void setup() {
 
 void loop() {
   // Check to see if we have received any commands
-  
-  if (Serial.available() > 0){
-    String command = Serial.readString();
-    command.trim();
+
+  int numBytes = Serial.available();
+  char buff[64];
+  if (readLine(Serial.read(), buff, 63) > 0){
     Serial.print("Echo: '");
-    Serial.print(command);
+    Serial.print(buff);
     Serial.println("'");
 
-    int command_int = command.toInt();
-    if (command_int >= 0){
-      x_stepper.setSpeed(command.toInt());
-      y_stepper.setSpeed(command.toInt());
-      music = 0;
-    } else {
+    // TODO: I'm pretty sure this is bad, unsafe code. Not sure how to fix it -Caleb
+    command_arg = strtok(buff, " ");
+    command_args[0] = command_arg;
+    int i = 1;
+    while (command_arg != NULL && i<10){
+      command_args[i] = command_arg;
+//      Serial.println(command_arg);
+      command_arg = strtok(NULL, " ");
+      i++;
+    }
+
+    if (strcmp(command_args[0], "music")==0){
       music = 1;
+    } else {
+      int command_int = atoi(command_args[0]);
+      x_stepper.setSpeed(command_int);
+      y_stepper.setSpeed(int(command_int*.667));
+      music = 0;
     }
   }
+
+  doAction();
+}
+
+char* getPos(){
+  char strBuff[50];
+  sprintf(strBuff, "(%.3f,%.3f)", altitude, azimuth);
+  return strBuff;
+}
+
+void doAction() {
   if(music){
     x_stepper.setSpeed(bass[int(millis()/1500)%8]);
     y_stepper.setSpeed(tenor[int(millis()/1500)%8]);
@@ -59,4 +87,27 @@ void loop() {
     x_stepper.stop();
     y_stepper.stop();
   }
+}
+
+int readLine(int readch, char *buffer, int len) {
+  // https://hackingmajenkoblog.wordpress.com/2016/02/01/reading-serial-on-the-arduino/
+    static int pos = 0;
+    int rpos;
+
+    if (readch > 0) {
+        switch (readch) {
+            case '\r': // Ignore CR
+                break;
+            case '\n': // Return on new-line
+                rpos = pos;
+                pos = 0;  // Reset position index ready for next time
+                return rpos;
+            default:
+                if (pos < len-1) {
+                    buffer[pos++] = readch;
+                    buffer[pos] = 0;
+                }
+        }
+    }
+    return 0;
 }
